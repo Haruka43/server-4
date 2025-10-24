706059a5564679214f993e9df362ba02953b584c
Haruka43 <206505340+Haruka43@users.noreply.github.com>
2025-10-24 12:10:29 +0900

    できた

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 // 自分で一から書いてみよう
+import { Hono } from 'jsr:@hono/hono';
+import { serveStatic } from 'jsr:@hono/hono/deno';
+const app = new Hono();
+
+app.use('/*', serveStatic({ root: './public' }));
+
+app.get('api', async (c) => {
+  // console.log('in the server side.');
+  const res = await fetch('https://randomuser.me/api/');
+  return c.json({ message: data });
+});
+
+Deno.serve(app.fetch);
